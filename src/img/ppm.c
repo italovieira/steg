138ecc472cc1857e4a1ea1b52c004edc60d0e2b1
Ítalo Vieira <italogv@hotmail.com>
2017-06-10 15:45:56 -0300

    Update ppm.c

--- src/img/ppm.c
+++ src/img/ppm.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include "ppm.h"
 
 void read()
 {
-  FILE *fp = fopen(".img/imd.ppm", "r");
+  FILE *fp = fopen("../../.img/imd.ppm", "rb");
 
   if (fp == NULL) {
-    puts("Unable to open file.");
+    perror("steg: cannot open 'FILE'");
   } else {
-    // Code for manipulate file in here
+    char c;
+      while ((c = fgetc(fp)) != EOF) {
+          putchar(c);
+      }
     fclose(fp);
   }
+}
+
+int main()
+{
+  read();
 }
