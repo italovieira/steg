b10727197ec4d97288e3cbd2c06814af334bd89f
Ítalo Vieira <italogv@hotmail.com>
2017-06-20 13:36:03 -0300

    Hide bits in pixels

--- src/img/ppm.c
+++ src/img/ppm.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "ppm.h"
 
 PPM* read()
@@ -41,4 +43,52 @@ PPM* read()
     fclose(fp);
     return img;
   }
+}
+
+bool* get_msg_bits(const char *msg, unsigned int msg_len)
+{
+  bool *bits = malloc(8 * (msg_len + 1) * sizeof *bits);
+  unsigned int bits_index = 0;
+
+  // Get bits sequence of msg and set to array bits
+  for (unsigned int i = 0; i < msg_len + 1; i++) {
+    for (short int j = 7; j >= 0; j--) {
+      bits[bits_index] = msg[i] >> j & 0x01;
+      bits_index++;
+    }
+  }
+  return bits;
+}
+
+// Change the lsb of byte RGB to bit
+void change_bit_lsb(unsigned char *byte, bool bit)
+{
+  *byte = bit ? *byte | 0x01 : *byte & 0xFE;
+}
+
+void change_pixels_lsb(PPM *img, unsigned int size_bits, bool bits[])
+{
+  unsigned int bits_index = 0;
+
+  for (unsigned int i = 0; i < img->x; i++) {
+    for (unsigned int j = 0; j < img->y; j++) {
+      unsigned char pixels[sizeof (Pixel)];
+      memcpy(pixels, &img->data[i][j], sizeof (Pixel));
+
+      for (unsigned short int k = 0; k < sizeof (Pixel)
+          && bits_index < size_bits; k++, bits_index++) {
+        change_bit_lsb(&pixels[k], bits[bits_index]);
+      }
+
+      memcpy(&img->data[i][j], pixels, sizeof (Pixel));
+    }
+  }
+}
+
+void hide_msg(PPM *img, const char *msg)
+{
+  unsigned int msg_len = strlen(msg), size_bits = (msg_len + 1) * 8;
+  bool *bits = get_msg_bits(msg, msg_len);
+
+  change_pixels_lsb(img, size_bits, bits);
 }
