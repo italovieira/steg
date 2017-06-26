bc54efc45331c24d86305ec16f38741b7b4d59eb
Ítalo Vieira <italogv@hotmail.com>
2017-06-26 19:12:20 -0300

    Fix bug when read BMP header

--- src/img/bmp.c
+++ src/img/bmp.c
@@ -13,19 +13,57 @@ BMP* read_bmp(const char *filename)
 
   // Read header
   Header *header = malloc(sizeof *header);
-  fread(header, 1, sizeof *header, fp);
+
+  fread(&header->type, sizeof header->type, 1, fp);
+  fread(&header->size, sizeof header->size, 1, fp);
+  fread(&header->reserved1, sizeof header->reserved1, 1, fp);
+  fread(&header->reserved2, sizeof header->reserved2, 1, fp);
+  fread(&header->offset, sizeof header->offset, 1, fp);
+
   if (ferror(fp)) {
     fprintf(stderr, "steg: error occured while reading file.\n");
     exit(EXIT_FAILURE);
   }
 
+  printf("%c%c ", header->type[0], header->type[1]);
+  printf("%u ", header->size);
+  printf("%u ", header->reserved1);
+  printf("%u ", header->reserved2);
+  printf("%u ", header->offset);
+  printf("\n");
+
   // Read header info
   HeaderInfo *header_info = malloc(sizeof *header_info);
-  fread(header_info, 1, sizeof *header_info, fp);
+
+  fread(&header_info->size, sizeof header_info->size, 1, fp);
+  fread(&header_info->x, sizeof header_info->x, 1, fp);
+  fread(&header_info->y, sizeof header_info->y, 1, fp);
+  fread(&header_info->planes, sizeof header_info->planes, 1, fp);
+  fread(&header_info->bit_count, sizeof header_info->bit_count, 1, fp);
+  fread(&header_info->compression, sizeof header_info->compression, 1, fp);
+  fread(&header_info->img_size, sizeof header_info->img_size, 1, fp);
+  fread(&header_info->xresolution, sizeof header_info->xresolution, 1, fp);
+  fread(&header_info->yresolution, sizeof header_info->yresolution, 1, fp);
+  fread(&header_info->colors_used, sizeof header_info->colors_used, 1, fp);
+  fread(&header_info->colors_important, sizeof header_info->colors_important, 1, fp);
+
   if (ferror(fp)) {
     fprintf(stderr, "steg: error occured while reading file.\n");
     exit(EXIT_FAILURE);
   }
+
+  printf("%d ", header_info->size);
+  printf("%d ", header_info->x);
+  printf("%d ", header_info->y);
+  printf("%d ", header_info->planes);
+  printf("%d ", header_info->bit_count);
+  printf("%d ", header_info->compression);
+  printf("%d ", header_info->img_size);
+  printf("%d ", header_info->xresolution);
+  printf("%d ", header_info->yresolution);
+  printf("%d ", header_info->colors_used);
+  printf("%d ", header_info->colors_important);
+  printf("\n");
 
   // Allocate img data
   Pixel **data = malloc(header_info->y * sizeof *data);
@@ -44,15 +82,6 @@ BMP* read_bmp(const char *filename)
 
   fclose(fp);
 
-  printf("%c%c ", header->type[0], header->type[1]);
-  printf("%u ", header->size);
-  printf("%u ", header->offset);
-  printf("\n");
-
-  printf("%d ", header_info->x);
-  printf("%d ", header_info->y);
-  printf("\n");
-
   BMP *img = malloc(sizeof *img);
   img->header = header;
   img->header_info = header_info;
@@ -69,17 +98,36 @@ void write_bmp(BMP *img, const char *filename)
     exit(EXIT_FAILURE);
   }
 
+
+  fwrite(&img->header->type, sizeof img->header->type, 1, fp);
+  fwrite(&img->header->size, sizeof img->header->size, 1, fp);
+  fwrite(&img->header->reserved1, sizeof img->header->reserved1, 1, fp);
+  fwrite(&img->header->reserved2, sizeof img->header->reserved2, 1, fp);
+  fwrite(&img->header->offset, sizeof img->header->offset, 1, fp);
+
   // Write header
-  if (fwrite(&img->header, 1, sizeof img->header, fp) != sizeof img->header) {
-    fprintf(stderr, "steg: error occured while writing file.\n");
-    exit(EXIT_FAILURE);
-  }
+  //if (fwrite(&img->header, 1, sizeof img->header, fp) != sizeof img->header) {
+  //  fprintf(stderr, "steg: error occured while writing file.\n");
+  //  exit(EXIT_FAILURE);
+  //}
+
+  fwrite(&img->header_info->size, sizeof img->header_info->size, 1, fp);
+  fwrite(&img->header_info->x, sizeof img->header_info->x, 1, fp);
+  fwrite(&img->header_info->y, sizeof img->header_info->y, 1, fp);
+  fwrite(&img->header_info->planes, sizeof img->header_info->planes, 1, fp);
+  fwrite(&img->header_info->bit_count, sizeof img->header_info->bit_count, 1, fp);
+  fwrite(&img->header_info->compression, sizeof img->header_info->compression, 1, fp);
+  fwrite(&img->header_info->img_size, sizeof img->header_info->img_size, 1, fp);
+  fwrite(&img->header_info->xresolution, sizeof img->header_info->xresolution, 1, fp);
+  fwrite(&img->header_info->yresolution, sizeof img->header_info->yresolution, 1, fp);
+  fwrite(&img->header_info->colors_used, sizeof img->header_info->colors_used, 1, fp);
+  fwrite(&img->header_info->colors_important, sizeof img->header_info->colors_important, 1, fp);
 
   // Write header_info
-  if (fwrite(&img->header_info, 1, sizeof img->header_info, fp) != sizeof img->header_info) {
-    fprintf(stderr, "steg: error occured while writing file.\n");
-    exit(EXIT_FAILURE);
-  }
+  //if (fwrite(&img->header_info, 1, sizeof img->header_info, fp) != sizeof img->header_info) {
+  //  fprintf(stderr, "steg: error occured while writing file.\n");
+  //  exit(EXIT_FAILURE);
+  //}
 
   // Write the pixels
   for (unsigned int i = 0; i < img->header_info->y; i++) {
